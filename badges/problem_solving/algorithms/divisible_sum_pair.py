3ff41a24e99b08e27de1aab6c19e3b0bc81b3e98
Md. Abu Zehad Antu <tantu249@gmail.com>
2022-04-13 21:02:51 +0600

    problem_solving_day_4

---
+++ badges/problem_solving/algorithms/divisible_sum_pair.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n , k , ar):
+    count = 0
+    for i in range(n):
+        for j in range(i + 1 , n):
+            if i < j and (i + j) % k == 0:
+                count += 1
+    return count
+
+
+if __name__ == '__main__':
+    result = divisibleSumPairs(6 , 3 , [1 , 3 , 2 , 6 , 1 , 2])
+    print(result)
