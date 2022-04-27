c371e63b0f04241d90e7e6c015dec98595bc8222
Md. Abu Zehad Antu <tantu249@gmail.com>
2022-04-28 04:08:09 +0600

    problem_solving_day_19

--- python/runner_up.py
+++ python/runner_up.py
@@ -1,3 +1,12 @@
 if __name__ == '__main__':
     arr = [2,3,6,6,5]
-    print(sorted(list(set(arr)))[-2])
+    max = float('-inf')
+    min = max
+
+    for i in set(arr):
+        if i > max:
+            min = max
+            max = i
+        elif min < i:
+            min = i
+    print(min)
